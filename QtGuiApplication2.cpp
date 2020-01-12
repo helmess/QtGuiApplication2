912c4c7c6ffd4fbb5914887f0aadcaa77f572d1d
charlie <yangchuanli000@qq.com>
2020-01-12 23:09:31 +0800

    添加新按钮1

--- QtGuiApplication2.cpp
+++ QtGuiApplication2.cpp
@@ -1,5 +1,6 @@
 #include "QtGuiApplication2.h"
 #include <qdebug.h>
+
 QtGuiApplication2::QtGuiApplication2(QWidget *parent)
 	: QMainWindow(parent)
 {
@@ -10,6 +11,15 @@ QtGuiApplication2::QtGuiApplication2(QWidget *parent)
 		{
 			qDebug() << "hello GIT" ;
 		});
+	QPushButton* btn1 = new QPushButton(this);
+	btn1->setText("click_2");
+	btn1->move( QPoint(0,100));
+	connect(btn1, &QPushButton::clicked, this, [=]()
+		{
+			qDebug() << "hello World";
+		});
+	
+
 
 
 }
