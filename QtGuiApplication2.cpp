8ba2d95ccf6f61dec544b2822ce5eab9c2b6686d
charlie <yangchuanli000@qq.com>
2020-01-12 22:53:58 +0800

    按钮测试

--- QtGuiApplication2.cpp
+++ QtGuiApplication2.cpp
@@ -1,10 +1,10 @@
 #include "QtGuiApplication2.h"
-
+#include <qdebug.h>
 QtGuiApplication2::QtGuiApplication2(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
-	QPushButton* btn = new QPushButton();
+	QPushButton* btn = new QPushButton(this);
 	btn->setText( "click");
 	connect(btn, &QPushButton::clicked, this, [=]()
 		{
